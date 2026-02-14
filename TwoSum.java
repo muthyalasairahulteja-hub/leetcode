37a61bf69f7f82a7d5571e8f2764d70966573bcb
muthyalasairahulteja-hub <muthyalasairahulteja@gmail.com>
2026-02-14 14:24:07 +0530

    Rename method twoSum to findtwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,5 @@
 class TwoSum{
-    public int[] twoSum(int[] nums, int target) {
+    public int[] findtwoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
